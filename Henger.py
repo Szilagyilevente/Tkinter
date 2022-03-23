ce314354e86d683264eebe9516165d522af89f63
Szilagyi Levente <78954849+Szilagyilevente@users.noreply.github.com>
2022-03-23 15:55:36 +0100

    alapok és kíszámítás gomb elkészítése

--- Henger.py
+++ Henger.py
@@ -1,2 +1,15 @@
 from tkinter import *
 foablak=Tk()
+
+Sugar=Label(foablak, text='Sugár (cm):')
+Sugar.grid()
+Sugarmezo=Entry(foablak)
+Sugarmezo.grid()
+Magassag=Label(foablak, text='Magasság (cm):')
+Magassag.grid()
+Magasmezo=Entry(foablak)
+Magasmezo.grid()
+Kiszámít=Button(foablak, text='Kiszámít', command=osszeg)
+Kiszámít.grid()
+
+foablak.mainloop()
